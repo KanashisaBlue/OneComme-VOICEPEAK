2513331e822d44a315e626ddd90c60ac6af9a240
Kanashisa <kanashisablue@gmail.com>
2023-12-17 17:30:48 +0900

    [v2.0.0] 公式WebSocket APIに対応したv2.0.0の説明文をコミットしました

--- onecomme_voicepeak_threading_ws.py
+++ onecomme_voicepeak_threading_ws.py
@@ -125,7 +125,7 @@ async def ws_connect():
     
     try:
         async with websockets.connect("ws://127.0.0.1:11180/sub") as websocket: #固定のため直書き
-            print('わんコメとの接続が完了しました。読み上げができる状態です')
+            print('わんコメとの接続が完了しました。')
             await ws_recv(websocket)
 
     except Exception as e:
